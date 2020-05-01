8791a2158c5f900c1edbfdb6168da3e02285abac
TRANSAERO21 <60730069+TRANSAERO21@users.noreply.github.com>
2020-05-01 15:23:52 +0300

    Add more COMMON_LUNCH_CHOICES

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,7 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/mokee_m1882.mk
+    $(LOCAL_DIR)/lineage_m1882.mk
 
 COMMON_LUNCH_CHOICES := \
-    mokee_m1882-userdebug
+    lineage_m1882-userdebug\
+	lineage_m1882-user\
+	lineage_m1882-eng
